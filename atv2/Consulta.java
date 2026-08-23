bacaf2888c7d2c08d5bedc58a1460444dacd3f0e
Ryan Correia <ryanhomero.2024@gmail.com>
2026-08-23 18:38:35 -0300

    Create Consulta class with appointment methods

---
+++ atv2/Consulta.java
@@ -0,0 +1,28 @@
+public class Consulta{
+    public LocalDate data;
+    public LocalTime hora;
+    public String medico;
+    public String paciente;
+    public String motivo;
+    public String historico;
+    
+    public void marcar(){
+        
+    }
+    
+    public void cancelar(){
+        
+    }
+ 
+    public void consultar(){
+        
+    } 
+    
+    public void realizar(){
+        
+    }
+
+    public void atualizar(){
+        
+    }
+    
