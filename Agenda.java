d86b91e99bf414ca8edfa75360f59ad0dce6ad7e
Ryan Correia <ryanhomero.2024@gmail.com>
2026-08-23 18:37:20 -0300

    Create Agenda class with basic attributes and method

---
+++ Agenda.java
@@ -0,0 +1,10 @@
+public class Agenda{
+    public LocalDate data;
+    public LocalTime hora;
+    public String medico;
+    public String paciente;
+    
+    public void consultar(){
+        
+    }
+}
