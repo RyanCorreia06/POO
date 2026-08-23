afb7caecb660c6bf038355cd0df8b076501fd657
Ryan Correia <ryanhomero.2024@gmail.com>
2026-08-23 18:41:05 -0300

    Create Receita class with consulta and data fields

---
+++ atv2/Receita.java
@@ -0,0 +1,14 @@
+public class Receita{
+    public String consulta;
+    public String data;
+    public String historico;
+    
+    public void preescrever(){
+        
+    }
+
+    public void consultar(){
+        
+    } 
+    
+}
