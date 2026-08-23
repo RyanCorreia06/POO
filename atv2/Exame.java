2f4c00a06ecbb3a4836b5c17b5e26bd463db41bf
Ryan Correia <ryanhomero.2024@gmail.com>
2026-08-23 18:39:20 -0300

    Create Exame class with basic structure

---
+++ atv2/Exame.java
@@ -0,0 +1,14 @@
+public class Exame{
+    public String consulta;
+    public String data;
+    public String historico;
+    
+    public void solicitar(){
+        
+    }
+
+    public void consultar(){
+        
+    } 
+    
+}
