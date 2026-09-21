71b9a9940a3652fd2d0916654010c3dba5ba7606
Ryan Correia <ryanhomero.2024@gmail.com>
2026-09-21 19:57:37 -0300

    refazendo com campos privados e getters e setters/ corrigido

--- atv2/Exame.java
+++ atv2/Exame.java
@@ -1,14 +1,30 @@
-public class Exame{
-    public String consulta;
-    public String data;
-    public String historico;
+public class Exame {
+  private Consulta consulta;
+    private String data;
+    private String descritivo;
+    public Consulta getConsulta() {
+        return consulta;
+    }
+    public void setConsulta(Consulta consulta) {
+        this.consulta = consulta;
+    }
+    public String getData() {
+        return data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+    public String getDescritivo() {
+        return descritivo;
+    }
+    public void setDescritivo(String descritivo) {
+        this.descritivo = descritivo;
+    }
     
-    public void solicitar(){
-        
+    public void mostrar() {
+        System.out.println( "Receita [consulta=" + consulta + ", data=" + data + ", descritivo=" + descritivo + "]");
     }
 
-    public void consultar(){
-        
-    } 
-    
+    public void preescrever(){}//todo
+    public void consultar(){}//todo
 }
