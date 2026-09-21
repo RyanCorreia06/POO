dc75a5527e5bf42e741dd57c5adb229deb9a98cb
Ryan Correia <ryanhomero.2024@gmail.com>
2026-09-21 19:41:05 -0300

    Refactor Agenda class with encapsulation and methods

--- atv2/Agenda.java
+++ atv2/Agenda.java
@@ -1,10 +1,40 @@
-public class Agenda{
-    public LocalDate data;
-    public LocalTime hora;
-    public String medico;
-    public String paciente;
+public class Agenda {
+    private String data;
+    private String hora;
+    private Medico medico;
+    private Paciente paciente;
+    
+    public String getData() {
+        return data;
+    }
+    public void setData(String data) {
+        this.data = data;
+    }
+    public String getHora() {
+        return hora;
+    }
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+    public Medico getMedico() {
+        return medico;
+    }
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+    public Paciente getPaciente() {
+        return paciente;
+    }
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+   
+    public void mostrar() {
+        System.out.println("Agenda [data=" + data + ", hora=" + hora + ", medico=" + medico + ", paciente=" + paciente + "]");
+    }
     
     public void consultar(){
-        
+        //TODO
     }
+
 }
