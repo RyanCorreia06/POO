728f54993e361c65ef2e1e78b4c0625e41896fd8
Ryan Correia <ryanhomero.2024@gmail.com>
2026-09-21 20:03:40 -0300

    Refactor Recepcionista class to use private fields

--- atv2/Recepcionista.java
+++ atv2/Recepcionista.java
@@ -1,10 +1,41 @@
-public class Recepcionista{
-    public String nome;
-    public String cpf;
-    public String telefone;
-    public String senha;
-    
+public class Recepcionista {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String senha;
+
     public void acessar(){
-        
+        //TODO
     }
+   
+    public void mostrar() {
+        System.out.println("Recepcionista [nome=" + nome + ", cpf=" + cpf + ", telefone=" + telefone + ", senha=" + senha + "]");
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getSenha() {
+        return "************";
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
 }
