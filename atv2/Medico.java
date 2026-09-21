72e27fb510ead33dd251f67f8c1b2d529966b3b1
Ryan Correia <ryanhomero.2024@gmail.com>
2026-09-21 19:58:23 -0300

    Refactor Medico class with encapsulation and new fields
    Refactor Medico class to use private fields and add getter/setter methods for telefone and especilidade.

--- atv2/Medico.java
+++ atv2/Medico.java
@@ -1,10 +1,49 @@
-public class Medico{
-    public String nome;
-    public String crm;
-    public String especialidade;
-    public String senha;
-    
-    public void acessar(){
-        
+public class Medico {
+    private String nome;
+    private String crm;
+    private String telefone;
+    private String especilidade;
+    private String senha;
+
+    public String getNome() {
+        return nome;
     }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCrm() {
+        return crm;
+    }
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getEspecilidade() {
+        return especilidade;
+    }
+    public void setEspecilidade(String especilidade) {
+        this.especilidade = especilidade;
+    }
+    public String getSenha() {
+        return "********";
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+    public void mostrar() {
+        System.out.println("Medico [nome=" + nome + ", crm=" + crm + ", telefone=" + telefone + ", especilidade=" + especilidade
+                + ", senha=" + senha + "]");
+    }
+
+    public void acessar(){
+        //TODO
+    }
+
 }
